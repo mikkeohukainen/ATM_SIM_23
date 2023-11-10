9dfa3a7099dde4218084acb19239556004e8f3e2
Miku Ohukainen <mikke.ohukainen@gmail.com>
2023-11-10 18:18:23 +0200

    customer model ja route tehty ja testattu

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var accountRouter = require('./routes/account');
+var customerRouter = require('./routes/customer');
 
 var app = express();
 
@@ -20,5 +21,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use('/account', accountRouter);
+app.use('/customer', customerRouter);
 
 module.exports = app;
---
+++ backend/models/customer_model.js
@@ -0,0 +1,28 @@
+const db = require('../database');
+
+const customer = {
+  getAll: function(callback) {
+    return db.query('select * from customer', callback);
+  },
+  getById: function(id, callback) {
+    return db.query('select * from customer where idcustomer=?', [id], callback);
+  },
+  add: function(customer, callback) {
+    return db.query(
+      'insert into customer (fname, lname) values(?,?)',
+      [customer.fname, customer.lname],
+      callback
+    );
+  },
+  delete: function(id, callback) {
+    return db.query('delete from customer where idcustomer=?', [id], callback);
+  },
+  update: function(id, customer, callback) {
+    return db.query(
+      'update customer set fname=?, lname=? where idcustomer=?',
+      [customer.fname, customer.lname, id],
+      callback
+    );
+  }
+};
+module.exports = customer;
