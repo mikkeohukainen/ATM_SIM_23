ca57e12c76117fd9e68102b9e2f9352ceb16141a
SanteriLehto02 <santeri.lehto@live.fi>
2023-11-13 18:01:30 +0200

    account_access model ja router

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { router as accountRouter } from './routes/account.js';
 import { router as customerRouter } from './routes/customer.js';
+import { router as account_accessRouter } from './routes/account_access.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -19,5 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/account', accountRouter);
 app.use('/customer', customerRouter);
+app.use('/account_access', account_accessRouter);
 
 export default app;
